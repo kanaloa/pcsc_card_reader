f7334b065a85e9467d99fe58f9da20c765343ca5
kodegear <kodegear@kodegear>
2016-04-05 21:57:32 +0900

    initial commit

---
+++ pcsc_card_reader/src/pcsc_card_reader.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : pcsc_card_reader.c
+ Author      : jeonbh
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
